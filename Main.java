8cfc065177da042a2756852a0c8bef950a2c65a9
Nathaniel Hoover <nathaniel.l.hoover@gmail.com>
2015-05-17 19:57:30 -0500

    Added a working getLatest method

--- Main.java
+++ Main.java
@@ -3,18 +3,20 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
+        //-------------------------------------------------------------------------------------------------------------
         //getLatest(String symbol, int type) type:1=Open, type:2=High, type:3=Low, type:4=Close, type:5=Volume, type:6=Adj Close
         //getLatest(String symbol) Gets all the newest data
         System.out.println("Print data for today");
-        HashMap<Integer, String[]> data =  YahooFinanceAPI.getLatest("AMD");
+        String[] data =  YahooFinanceAPI.getLatest("AMD");
 
-        String[] currentLine;
-        for(int i = 0; i < data.size(); i++){
-            currentLine = data.get(i);
-            for(int j = 0; j < currentLine.length; j++){
-                System.out.print(currentLine[j] + "\t|");
-            }
-            System.out.println();
+        for(int i = 0; i < data.length; i++){
+           System.out.print(data[i] + " | ");
         }
+        System.out.println();
+
+        System.out.println("Latest Close: " + YahooFinanceAPI.getLatest("AMD", 4));
+        //-------------------------------------------------------------------------------------------------------------
+        //getRange(String symbol, String fromDay, String fromMonth, String fromYear, String toDay, String toMonth, String toYear, String frequency, int type)
+        //getRange(String symbol, String fromDay, String fromMonth, String fromYear, String toDay, String toMonth, String toYear, String frequency)
     }
 }
